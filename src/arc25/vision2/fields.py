fixed mixed precision and variance blow-up on initialisation

src/arc25/vision2/fields.py
@@ -296,6 +296,7 @@ class FieldDims:
         *,
         shape: tuple[int, int] | None = None,
         sym_decomp_cls: SymDecompBase = SplitSymDecomp,
+        dtype=jnp.float32,
     ) -> Field:
         if shape is None:
             shape = self.shape
@@ -304,12 +305,14 @@ class FieldDims:
             assert self.shape is None or shape == self.shape
         Y, X = shape
         ret = Field(
-            context=sym_decomp_cls.empty(self.context, batch + (self.context_tokens,)),
-            cells=sym_decomp_cls.empty(self.cells, batch + shape),
+            context=sym_decomp_cls.empty(
+                self.context, batch + (self.context_tokens,), dtype=dtype
+            ),
+            cells=sym_decomp_cls.empty(self.cells, batch + shape, dtype=dtype),
             grid=CoordinateGrid(
-                ypos=np.empty(batch + (Y, 2)),
-                xpos=np.empty(batch + (X, 2)),
-                mask=np.empty(batch + (Y, X), bool),
+                ypos=jnp.empty(batch + (Y, 2), dtype=dtype),
+                xpos=jnp.empty(batch + (X, 2), dtype=dtype),
+                mask=jnp.empty(batch + (Y, X), dtype=bool),
             ),
         )
         assert self.validate(ret), self.validation_problems(ret)

src/arc25/vision2/linear.py
@@ -148,6 +148,7 @@ class SpaceSymmetricTensor(nnx.Module):
         symmetry_group: type[SymOpBase] = D4,
         dtype: Dtype = jnp.float32,
         init: Initializer = default_kernel_init,
+        init_scale: float = 1,
         rngs: rnglib.Rngs,
     ):
         if not isinstance(covariant, tuple):
@@ -181,8 +182,23 @@ class SpaceSymmetricTensor(nnx.Module):
         self.mapping_info = nnx_compat.static(mapping_info)
         self.transpose_dims = transpose_dims
         self.trivial_shape = tuple(trivial_shape)
+        # TODO: how do we teach a general init what is inputs and what is outputs?
+        if init is default_kernel_init:
+            n_in = max(
+                1,
+                np.prod(
+                    [
+                        len(r) if not isinstance(r, int) else r
+                        for r, c in zip(representations, covariant)
+                        if c
+                    ]
+                ),
+            )
+            init = jax.nn.initializers.truncated_normal(stddev=np.sqrt(0.1 / n_in))
+
         self.params = nnx.Param(
-            (init if all(trivial_shape) else jax.nn.initializers.zeros)(
+            init_scale
+            * (init if all(trivial_shape) else jax.nn.initializers.zeros)(
                 rngs.params(),
                 (mapping_info.n_free_subspaces,) + tuple(trivial_shape),
                 dtype,
@@ -242,6 +258,7 @@ class SpaceSymmetricLinear(nnx.Module):
         param_dtype: Dtype = jnp.float32,
         precision: PrecisionLike = None,
         kernel_init: Initializer = default_kernel_init,
+        init_scale: float = 1,
         bias_init: Initializer = default_bias_init,
         dot_general: DotGeneralT = lax.dot_general,
         promote_dtype: PromoteDtypeFn = dtypes.promote_dtype,
@@ -282,6 +299,7 @@ class SpaceSymmetricLinear(nnx.Module):
             symmetry_group=symmetry_group,
             dtype=param_dtype,
             init=kernel_init,
+            init_scale=init_scale,
             rngs=rngs,
         )
 
@@ -324,6 +342,7 @@ class SymDecompLinear(nnx.Module):
         precision: PrecisionLike = None,
         mode: typing.Literal["flat", "split"] | None = None,
         kernel_init: Initializer = default_kernel_init,
+        init_scale: float = 1,
         bias_init: Initializer = default_bias_init,
         dot_general: DotGeneralT = lax.dot_general,
         promote_dtype: PromoteDtypeFn = dtypes.promote_dtype,
@@ -338,10 +357,15 @@ class SymDecompLinear(nnx.Module):
             dtype=dtype,
             param_dtype=param_dtype,
             precision=precision,
+            promote_dtype=promote_dtype,
+        )
+        ikw = dict(
             kernel_init=kernel_init,
             bias_init=bias_init,
+            init_scale=init_scale,
             dot_general=dot_general,
-            promote_dtype=promote_dtype,
+            use_bias=False,
+            rngs=rngs,
         )
 
         self.in_features = in_features
@@ -381,9 +405,8 @@ class SymDecompLinear(nnx.Module):
                             extra_out_reps
                             + ((outf.rep.space,) if ko == "space" else ()),
                             vo,
-                            use_bias=False,
-                            rngs=rngs,
                             **kw,
+                            **ikw,
                         )
                         for ko, vo in outf.representations.items()
                         if {ki, ko} != {"space", "flavour"}
@@ -409,9 +432,8 @@ class SymDecompLinear(nnx.Module):
                 vi,
                 extra_out_reps,
                 vo,
-                use_bias=False,
-                rngs=rngs,
                 **kw,
+                **ikw,
             )
             if vi and vo
             else nnx_compat.data(None)
@@ -528,7 +550,7 @@ class SymDecompLinear(nnx.Module):
             flavour=xfa,
         )
         out = (
-            {k: v.get_tensor() for k, v in self.bias.items()}
+            {k: v.get_tensor(dtype=dtype) for k, v in self.bias.items()}
             if self.bias is not None
             else {}
         )

src/arc25/vision2/symrep.py
@@ -56,11 +56,11 @@ class SymDecompBase(abc.ABC, AttrsModel):
 
     def batch_reshape(self, *new_batch_shape) -> Self:
         cur_batch_shape = self.batch_shape
-        n_elements = np.prod(cur_batch_shape)
+        n_elements = int(np.prod(cur_batch_shape))
         if n_unspec := sum(v < 0 for v in new_batch_shape):
             if n_unspec > 1:
                 raise ValueError(f"Got more than one ({n_unspec}) -1 values")
-            n_rem = np.prod([v for v in new_batch_shape if v > 0])
+            n_rem = int(np.prod([v for v in new_batch_shape if v > 0]))
             assert n_elements == n_rem or (n_rem > 0 and not n_elements % n_rem)
             new_batch_shape = tuple(
                 v if v >= 0 else n_elements // n_rem if n_rem > 0 else 0
@@ -133,11 +133,13 @@ class SplitSymDecomp(SymDecompBase):
     rep: RepSpec = attrs.field(default=standard_rep, metadata=dict(static=True))
 
     @classmethod
-    def empty(cls, dims: SymDecompDims, batch: tuple[int, ...] = ()) -> Self:
+    def empty(
+        cls, dims: SymDecompDims, batch: tuple[int, ...] = (), *, dtype=jnp.float32
+    ) -> Self:
         ret = cls(
-            invariant=np.empty(batch + (dims.invariant,)),
-            space=np.empty(batch + (dims.n_space, dims.space)),
-            flavour=np.empty(batch + (dims.n_flavours, dims.flavour)),
+            invariant=jnp.empty(batch + (dims.invariant,), dtype=dtype),
+            space=jnp.empty(batch + (dims.n_space, dims.space), dtype=dtype),
+            flavour=jnp.empty(batch + (dims.n_flavours, dims.flavour), dtype=dtype),
             rep=dims.rep,
         )
         assert dims.validate(ret), dims.validation_problems(ret)